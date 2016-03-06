a5362b455a2e23c8f3f33deabf724b24807fd1cb
Martin Fahy <martinfa@buffalo.edu>
2016-03-06 07:30:55 -0500

    Circle, Updated To Dynamic Geo-Location

--- src/app.js
+++ src/app.js
@@ -6,9 +6,9 @@ var Accel = require('ui/accel');
 var Vector2 = require('vector2');
 var Settings = require('settings'); 
 
-
+var id; 
 // Set for Navigivation // 
-var options = {
+var locationOptions = {
   enableHighAccuracy: true, 
   timeout: 5000 
 }; 
@@ -47,8 +47,29 @@ var textfield = new UI.Text({
 mainActivity.add(textfield);
 mainActivity.show();
 
-/*
-Accel.peek(function(e){ 
+var pos = textfield.position(); 
+pos.y += 20; 
+textfield.animate('position', pos, 1000);
+pos.x -= 20; 
+textfield.animate('position', pos, 1000); 
+pos.x += 20; 
+textfield.animate('position', pos, 1000);
+
+var circle = new UI.Circle({
+  position: new Vector2(72, 55), 
+  radius: 25, 
+  brackgroundColor: 'red', 
+});
+mainActivity.add(circle); 
+mainActivity.show()
+
+var rad = circle.radius(); 
+rad -= 25;
+circle.animate('radius', rad, 10000); 
+mainActivity.hide(circle); 
+
+
+/*Accel.peek(function(e){ 
   console.log("This is axis: " + e.accel.axis + "Direction: " + e.accel.direction); 
   console.log("This is the x: " + e.accel.x + " " + "This is the Y: " + e.accel.y); 
 }); 
@@ -60,73 +81,32 @@ Accel.on('data', function(e) {
 Accel.on('tap', function(e) {
   console.log('Tap event on axis: ' + e.axis + ' and direction: ' + e.direction);
   Settings.data('Tap', e.transcription);
-});
-*/ 
+});*/
 
 Pebble.addEventListener('appmessage', function(e) {
   console.log('Recieved Message' + JSON.stringify(e.payload)); 
 });
 
-function success(pos)
+var latitude, longitude; 
+function locationSuccess(pos)
 {
-  var lat = pos.coords.latitude; 
-  var long = pos.coords.longitude; 
+  latitude = pos.coords.latitude; 
+  longitude = pos.coords.longitude; 
 }
 
-function error(err)
+function locationError(err)
 {
   console.warn('Error(' + err.code + '):' + err.message); 
 }
-navigator.geolocation.getCurrentPosition(success, error, options); 
 
-var transactionId = Pebble.sendAppMessage({ '0': lat, '1': 'Location of Geo-Coords'}, 
-                                         function(e) { console.log('Sent location'); });
-
- 
-
-/*
-mainActivity.on('click', 'down', function(e){
-  Voice.dictate('start', false, function(e){
-    if(e.err)
-    {  
-      console.warn('Error: ' + e.err); 
-      Pebble.showSimpleNotificationOnPebble('Coolcumber', 'Setting Phrase Failed.'); 
-      return; 
-    }
-    console.log(e.transcription); 
-    Pebble.showSimpleNotificationOnPebble('Coolcumber', 'Setting Phrase Worked.'); 
-    Settings.data('user_phrase', e.transcription);  
-  });
+Pebble.addEventListener('ready',
+  function(e) {
+    id = navigator.geolocation.watchPosition(locationSuccess, locationError, locationOptions);    
 }); 
-*/
 
+//navigator.geolocation.getCurrentPosition(success, error, options); 
 
-// pebble.addEventListener('click', 'down', function(e) {
-//     console.log('At least pressed')
-//     Voice.dictate('start', false, function(e){
-//       if(e.err)
-//       {
-//         console.warn('Error: ' + e.err); 
-//         Pebble.showSimpleNotificationOnPebble('Coolcumber', 'Setting Phrase Failed.'); 
-//         return; 
-//       }
-//       console.log(e.transcription); 
-//       //Pebble.showSimpleNotificationOnPebble('Coolcumber', 'Setting Phrase Worked.'); 
-//       Settings.data('user_phrase', e.transcription);  
-//   });
-// });
+var transactionId = Pebble.sendAppMessage({ '0': latitude, '1': longitude}, 
+                                         function(e) { console.log('Successfully delivered message with transactionId=' + e.data.transactionId) }, 
+                                         function(e) { console.log('Unable to deliver message with transactionId=' + e.data.transactionId + ' Error is: ' + e.data.error.message)});
 
-// pebble.addEventListener('click', 'up', function(e){
-//   var card = new UI.Card();
-//   card.title('A Card');
-//   card.subtitle('Is a Window');
-//   card.body('The simplest window type in Pebble.js.');
-//   card.show();
-// }); 
-
-
-// Accel.peek(function(e){
-//   if(e.accel.x >= 10 || e.accel.y >= 10) {
-//     console.log('send text'); 
-//   }
-// }); 
